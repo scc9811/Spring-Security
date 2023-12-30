4fde1d762d2664c8416f8aca438480fe9dfbbab7
scc9811 <101318479+scc9811@users.noreply.github.com>
2023-12-30 21:15:27 +0900

    /api/v1/users/login 접근허용

--- src/main/java/com/example/SecurityProject/config/SecurityConfig.java
+++ src/main/java/com/example/SecurityProject/config/SecurityConfig.java
@@ -1,27 +1,51 @@
 package com.example.SecurityProject.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig{
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/api/v1/user/login").permitAll()
-                        .anyRequest().authenticated()
-                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults());
+                .authorizeHttpRequests(
+                        httpRequests -> httpRequests
+                                .requestMatchers("/api/v1/users/login").permitAll()
+                                .anyRequest().authenticated()
+                );
+
         return http.build();
     }
+
+
 }
+
+
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(Customizer.withDefaults())
+//                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/api/v1/users/login").permitAll()
+//                        .anyRequest()
+//                        .authenticated()
+//                )
+//                .httpBasic(Customizer.withDefaults())
+//                .formLogin(Customizer.withDefaults());
+//        return http.build();
+//    }
+//}
